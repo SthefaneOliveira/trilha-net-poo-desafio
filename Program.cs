4457a5c76eed5c6634e8c04e3e51e7000e91b4eb
Sthefane Oliveira <sthefane.nehamah@gmail.com>
2023-08-29 18:29:24 -0300

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone1 = new Iphone("1111-2222", "iphone 14pro max", "1111", 364);
+Nokia nokia1 = new Nokia("5555-6666", "Nokia Tijolao", "9999", 64);
+
+nokia1.mostrarModelo();
+nokia1.Ligar();
+nokia1.InstalarAplicativo("facebook");
+
+Console.WriteLine("Iphone");
+iphone1.Ligar();
+iphone1.InstalarAplicativo("Instagram");
+
+
